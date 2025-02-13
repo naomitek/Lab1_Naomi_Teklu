5dae9ca3864ed7147e00ab1ad5f7d957ae1052f9
Naomi Teklu <101441399@georgebrown.ca>
2025-02-13 17:57:25 -0500

    Initial commit: Create Xcode project

---
+++ Lab1_Naomi_Teklu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_Naomi_Teklu
+//
+//  Created by usr on 2025-02-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_Naomi_Teklu/Lab1_Naomi_TekluApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Naomi_TekluApp.swift
+//  Lab1_Naomi_Teklu
+//
+//  Created by usr on 2025-02-13.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Naomi_TekluApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
